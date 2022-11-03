8fbcc5bd4c97873c810a3a58d1d0cea33f14a5c4
Andrey <dovban.andreyy@rambler.ru>
2022-11-03 16:09:27 +0300

    Create ToggleTheme , SvgCkmponent by change theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
-import "./App.css"
+import "./App.css";
+import { useAppSelector } from "./store/customHooks"; // Кастомный хук редакса для управления состоянием
+import ToggleTheme from "./components/ToggleTheme"; // Компонент переключатель темы
 
-export default function App(){
-  let out= <div>
-    <button className="btn btn-xs">Hello!!!</button>
-  </div>;
+export default function App() {
+  const theme = useAppSelector((state) => state.app.theme); // Состояние темы
 
-  return out
-} 
+  let out = (
+    <div
+      className="flex flex-col h-full items-center p-10"
+      data-theme={theme ? "luxury" : "fantasy"}
+    >
+      TODO LIST
+      <ToggleTheme />
+    </div>
+  );
+
+  return out;
+}
